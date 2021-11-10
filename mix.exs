957e42e3ae930facb1fc2a524fa9c161f6a5f45f
gogohhh <gogo_hhh_d@hotmail.com>
2021-11-10 14:20:46 -0600

    Se agrego live_dashborad para ver comportamiento

--- mix.exs
+++ mix.exs
@@ -33,16 +33,17 @@ defmodule DiegoApp.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.9"},
-      {:phoenix_live_view, "~> 0.15.1"},
+      {:phoenix, "~> 1.6.2"},
+      {:phoenix_live_view, "~> 0.17.3"},
       {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_html, "~> 2.11"},
+      {:phoenix_html, "~> 3.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:phoenix_live_dashboard, "~> 0.5"}
     ]
   end
 
